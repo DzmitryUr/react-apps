0fc8f33e0841c29347f090c7f498b445ec7d2668
Dzmitry <dimaau1@gmail.com>
2024-04-23 18:59:33 +0300

    add counter app

---
+++ counter/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Counter from "./components/counter";
+
+function App() {
+  return (
+    <>
+      <h1>React Counter App</h1>
+      <Counter />
+      <hr />
+      <Counter />
+      <hr />
+      <Counter />
+    </>
+  );
+}
+
+export default App;

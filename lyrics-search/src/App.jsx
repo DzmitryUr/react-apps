6a4e340e50bccd6e87a89e84dfd1176cb94b6eac
Dzmitry <dimaau1@gmail.com>
2024-08-12 14:42:11 +0300

    lyrics-search app

---
+++ lyrics-search/src/App.jsx
@@ -0,0 +1,8 @@
+import LyricsSearch from './components/LyricsSearch';
+import './App.css';
+
+function App() {
+  return <LyricsSearch url='https://api.lyrics.ovh/v1' />;
+}
+
+export default App;
---
+++ lyrics-search/src/components/LyricsSearch.jsx
@@ -0,0 +1,57 @@
+import './LyricsSearch.css';
+import Axios from 'axios';
+import { useState } from 'react';
+
+function LyricsSearch({ url }) {
+  const [artist, setArtist] = useState('');
+  const [song, setSong] = useState('');
+  const [lyrics, setLyrics] = useState('');
+  const [error, setError] = useState('');
+
+  const searchLyrics = async (event) => {
+    event.preventDefault();
+    if (artist === '' || song === '') {
+      return;
+    }
+    setLyrics('');
+    setError('');
+    try {
+      const res = await Axios.get(`${url}/${artist}/${song}`);
+      console.log(res.data.lyrics);
+      setLyrics(res.data.lyrics);
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className='search-container'>
+      <h2>Lyrics Finder</h2>
+      <form onSubmit={searchLyrics}>
+        <input
+          type='text'
+          placeholder='Artist name'
+          onChange={(e) => {
+            setArtist(e.target.value);
+          }}
+          required
+        />
+        <input
+          type='text'
+          placeholder='Song name'
+          onChange={(e) => {
+            setSong(e.target.value);
+          }}
+          required
+        />
+        <button type='submit'>Search</button>
+      </form>
+      <hr />
+      {error && <h2 className='error'>{error}</h2>}
+      <pre>{lyrics}</pre>
+    </div>
+  );
+}
+
+export default LyricsSearch;

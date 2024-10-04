d5e58249b69ace4a2c671e4ac51409532887902f
Dzmitry <dimaau1@gmail.com>
2024-10-04 20:11:25 +0300

    add openweather api (#3)

--- weather-app-tailwind/src/App.jsx
+++ weather-app-tailwind/src/App.jsx
@@ -1,8 +1,19 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query';
 import './App.css';
 import Weather from './components/Weather';
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <Weather />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Weather />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- weather-app-tailwind/src/components/Weather.jsx
+++ weather-app-tailwind/src/components/Weather.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import { useFetchWeather } from '../hooks/useFetchWeather';
 import useGeolocation from '../hooks/useGeolocation';
 
 export default function Weather() {
   const { loading, error, data: geoData } = useGeolocation();
+  const [city, setCity] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const {
+    data,
+    error: apiError,
+    isLoading: apiLoading,
+  } = useFetchWeather(geoData, searchQuery);
 
   if (loading) {
     return <p className='text-blue-500 text-lg font-semibold'>Loading ...</p>;
@@ -15,17 +24,63 @@ export default function Weather() {
     );
   }
 
+  const { currentWeather, forecast } = data || {};
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (city.trim()) {
+      console.log('city=', city);
+      setSearchQuery(city.trim());
+    }
+  };
+
   return (
-    <div className='bg-gradient-to-b from-blue-400 to-indigo-600 shadow-lg rounded-lg p-6 mt-10 text-white'>
-      <h2 className='text-3xl font-bold text-gray-800 mb-4'>
-        Your coordinates
-      </h2>
-      <p className='text-lg'>
-        <span className='font-semibold'>Latitude: {geoData.latitude}</span>
-      </p>
-      <p className='text-lg'>
-        <span className='font-semibold'>Longitude: {geoData.longitude}</span>
-      </p>
+    <div>
+      {error && <p>{error.message}</p>}
+      {apiError && <p>{apiError.message}</p>}
+      <form onSubmit={handleSearch}>
+        <input
+          type='text'
+          placeholder='Enter city name'
+          className='p-2 border border-gray-300 rounded'
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button
+          type='submit'
+          className='ml-2 p-2 bg-blue-500 text-white rounded'
+        >
+          Search
+        </button>
+      </form>
+      {currentWeather && (
+        <div className='mb-5 mt-5 text-center'>
+          <h2 className='text-xl font-semibold mb-2'>
+            Current Weather for {currentWeather.name}
+          </h2>
+          <p className='text-2xl font-bold'>
+            {Math.round(currentWeather.main.temp)}&deg;C
+          </p>
+          <p className='capitalize'>{currentWeather.weather[0].description}</p>
+        </div>
+      )}
+
+      {forecast && (
+        <div className='bg-blue-100 p-4 rounded-lg shadow-lg'>
+          <h2 className='text-lg font-bold mb-4'>Forecast</h2>
+          <ul className='space-y-4'>
+            {forecast.list.slice(0, 5).map((forecastItem, index) => (
+              <li key={index} className='p-4 bg-white rounded-lg shadow-md'>
+                <p className='text-lg font-semibold'>{forecastItem.dt_txt}</p>
+                <p className='text-2xl font-bold'>
+                  {Math.round(forecastItem.main.temp)}&deg;C
+                </p>
+                <p>Weather: {forecastItem.weather[0].description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
